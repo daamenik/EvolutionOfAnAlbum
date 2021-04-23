0419b1213d246eee14fcef71016763c4fef8b966
Dom Giandinoto <domgiandinoto@gmail.com>
2021-04-23 02:10:23 -0500

    fixing scroll thing

--- pages/js/functions.js
+++ pages/js/functions.js
@@ -303,12 +303,13 @@ function loadArtifacts(song) {
     }
 }
 
+// fade on scroll
 $(window).on("load",function() {
     $(window).scroll(function() {
       var windowBottom = $(this).scrollTop() + $(this).innerHeight();
       $(".fade").each(function() {
         /* Check the location of each desired element */
-        var objectBottom = $(this).offset().top + $(this).outerHeight();
+        var objectBottom = $(this).offset().top + ($(this).outerHeight() * 0.75);
         
         /* If the element is completely within bounds of the window, fade it in */
         if (objectBottom < windowBottom) { //object comes into view (scrolling down)
